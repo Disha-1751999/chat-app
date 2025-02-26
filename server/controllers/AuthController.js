ff1db01325ad08a08dc8167ed30a71039c68953c
Disha-1751999 <80054827+Disha-1751999@users.noreply.github.com>
2025-02-26 18:46:37 +0600

    updated

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,6 @@
 import { TokenEncode } from '../utilities/tokenUtility.js';
 import UserModel from './../models/UserModel.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import {renameSync,unlinkSync} from "fs"
 
 export const Register=async(req,res)=>{
